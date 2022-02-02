34a895781fe975952ea99530585b11b64eed26ff
ardi <ardi.firmansyah@alodokter.com>
2022-02-02 22:11:45 +0700

    Handle loading and empty

---
+++ components/NoData.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+const NoData: FC = () => (
+  <div className="flex h-56 items-center justify-center border border-gray-300 text-lg font-bold">
+    No Data
+  </div>
+);
+
+export default NoData;
